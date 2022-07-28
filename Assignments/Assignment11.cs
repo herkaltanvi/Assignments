2fac00066a7ca547efcbbdddfc0b4c54302193dd
THL2 <THL2@LP15-THL2-IND>
2022-07-28 10:06:39 +0530

    User input feature added - Assignment 11

--- Assignments/Assignment11.cs
+++ Assignments/Assignment11.cs
@@ -17,6 +17,17 @@ namespace Assignments
             arr[0] = new int[3] { 2, 3, 0 };
             arr[1] = new int[3] { 5, 8, 2 };
             arr[2] = new int[3] { 1, 4, 9 };
+
+
+
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    Console.Write($"Enter element {j + 1} of Array {i + 1} : ");
+                    arr[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
             Console.WriteLine("# Middle Elements of an Array #");
             for (int i = 0; i < 3; i++)
             {
