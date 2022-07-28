b5d02d47a75df24ba9852bb24187728c958bb3fe
THL2 <THL2@LP15-THL2-IND>
2022-07-28 09:35:19 +0530

    assignment 11 done!

--- Assignments/Assignment11.cs
+++ Assignments/Assignment11.cs
@@ -11,17 +11,22 @@ namespace Assignments
         public void Assignment()
         {
 
-            /*int[] res = new int[3];
-            int[][] arr = new int[3][3];
+            int[] res = new int[3];
+            int[][] arr = new int[3][];
 
             arr[0] = new int[3] { 2, 3, 0 };
-            int[] arr2 = new int[3] { 5, 8, 2 };
-            int[] arr3 = new int[3] { 1, 4, 9 };
+            arr[1] = new int[3] { 5, 8, 2 };
+            arr[2] = new int[3] { 1, 4, 9 };
             Console.WriteLine("# Middle Elements of an Array #");
             for (int i = 0; i < 3; i++)
             {
-                res[i] = arr1
-            }*/
+                res[i] = arr[i][1];
+            }
+            for (int i=0;i<3;i++)
+            {
+                Console.Write($"{res[i]}  ");
+            }
+            Console.WriteLine("");
         }
     }
 
